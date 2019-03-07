03fb08a4fd06312144ad90bae64f5ed802463429
Alex King <alexanderddking@gmail.com>
2019-03-07 15:31:41 +0900

    Removed Hotjar

--- src/html.js
+++ src/html.js
@@ -63,17 +63,6 @@ export default class HTML extends React.Component {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MG2VTBL');
           ` }} />
-          {/* <script dangerouslySetInnerHTML={{ __html: `
-            // Hotjar Tracking Code for https://www.exolaunchpad.com
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:1175389,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          ` }} /> */}
         </body>
       </html>
     )

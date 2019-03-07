06163195fe639e9d525efad10d0a06ae083b850f
Alex King <alexanderddking@gmail.com>
2019-03-07 14:56:45 +0900

    GTag manager

--- src/html.js
+++ src/html.js
@@ -57,12 +57,13 @@ export default class HTML extends React.Component {
           {this.props.postBodyComponents}
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133345282-1"></script>
           <script  dangerouslySetInnerHTML={{ __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-133345282-1');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MG2VTBL');
           ` }} />
-          <script dangerouslySetInnerHTML={{ __html: `
+          {/* <script dangerouslySetInnerHTML={{ __html: `
             // Hotjar Tracking Code for https://www.exolaunchpad.com
             (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -72,7 +73,7 @@ export default class HTML extends React.Component {
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          ` }} />
+          ` }} /> */}
         </body>
       </html>
     )

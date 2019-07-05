bce8f837dc3bda712aa2c859396895ca108e7371
Ernesto Serrano <info@ernesto.es>
2019-07-05 11:50:59 +0200

    fix css

--- src/html.js
+++ src/html.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import config from "../gatsby-config"
+import { withPrefix } from 'gatsby'
 
 export default class HTML extends React.Component {
   render() {
